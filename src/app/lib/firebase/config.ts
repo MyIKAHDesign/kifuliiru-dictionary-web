143be4bbdd98cfc37bfb0144f2ea13c57109e5f0
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-11-06 13:50:12 -0800

    Change config

--- src/app/lib/firebase/config.ts
+++ src/app/lib/firebase/config.ts
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
