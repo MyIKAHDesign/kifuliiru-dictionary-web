aec06d807afa78e31ed341504e83d8c8d2461517
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-12-11 15:45:58 -0800

    Add styling stats to be 2 columns

--- src/app/components/statsSection.tsx
+++ src/app/components/statsSection.tsx
@@ -48,7 +48,7 @@ export default function StatsSection() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           {stats.map((stat, index) => (
             <StatsCard
               key={stat.title}
